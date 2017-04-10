6b39ca7a6cd4f87dc2fc6cdd14c64b4f83e2b680
fanamb <abellais@PC799.alteca.fr>
2017-04-10 14:58:00 +0200

    ajout

--- Serie01/src/Champi.java
+++ Serie01/src/Champi.java
@@ -3,32 +3,41 @@ import java.util.Scanner;
 public class Champi {
 
 	public static void main(String[] args) {
+		Boolean lamelles, anneau, chapeau, foret;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
 		System.out.println("cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
 
-		System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
-		Boolean lamelles = sc.nextBoolean();
+
 		System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
-		Boolean anneau = sc.nextBoolean();
-		System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
-		Boolean chapeau = sc.nextBoolean();
-		System.out.print("Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ");
-		Boolean forêt = sc.nextBoolean();
+		anneau = sc.nextBoolean();
+		if(anneau) {
+			System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
+			chapeau = sc.nextBoolean();
+			System.out.print("Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ");
+			foret = sc.nextBoolean();
+			lamelles = true;
+		} else {
+			System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
+			lamelles = sc.nextBoolean();
+			System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
+			chapeau = sc.nextBoolean();
+			foret = true;
+		}
 
 		System.out.print("==> Le champignon auquel vous pensez est ");
 
-		if(lamelles && anneau && chapeau && !forêt) {
+		if(lamelles && anneau && chapeau && !foret) {
 			System.out.print("l'agaric jaunissant");
-		} else if(lamelles && anneau && chapeau && forêt) {
+		} else if(lamelles && anneau && chapeau && foret) {
 			System.out.print("l'amanite tue-mouches");
-		}  else if(!lamelles && !anneau && !chapeau && forêt) {
+		}  else if(!lamelles && !anneau && !chapeau && foret) {
 			System.out.print("le cèpe de Bordeaux");
-		} else if(!lamelles && !anneau && chapeau && forêt) {
+		} else if(lamelles && anneau && !chapeau && !foret) {
 			System.out.print("le coprin chevelu");
-		} else if(lamelles && !anneau && !chapeau && forêt) {
+		} else if(lamelles && !anneau && !chapeau && foret) {
 			System.out.print("la girolle");
-		} else if(lamelles && !anneau && chapeau && forêt) {
+		} else if(lamelles && !anneau && chapeau && foret) {
 			System.out.print("le pied bleu");
 
 		}
